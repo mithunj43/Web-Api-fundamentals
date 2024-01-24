6363967e419bb2e6765b02f0f7ad0fd180e47bc1
mithun <mithunj43@gmail.com>
2024-01-24 19:50:40 +0530

    Adding support for JsonPatchdocument and some gitnore files

--- CityInfo/CityInfo.API/Program.cs
+++ CityInfo/CityInfo.API/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options =>
 { 
     options.ReturnHttpNotAcceptable = true; //if the requested format is not found
-}).AddXmlDataContractSerializerFormatters(); //Support ths xml format
+}).AddNewtonsoftJson()
+.AddXmlDataContractSerializerFormatters(); //Support ths xml format
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
